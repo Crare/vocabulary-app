eb1b89d36bbdcf8c8ebf4c615e94db0a1491d171
Juho Heikkinen <juho.heikkinen@zure.com>
2024-05-23 20:49:30 +0300

    add materia-ui

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="container">
+      <h1>Vocabulary-app</h1>
+      <p>Memorize any vocabulary by repetition.</p>
+    </div>
+  );
+};
+
+export default Main;
+
